1ef66d94822f7a53ec3d07025d0f8a1463dd7d88
Kelson <kelsonbatista@gmail.com>
2024-11-06 16:24:49 -0300

    added gradio and basic structure

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,20 @@
 import os
 
 import google.generativeai as genai
+import gradio
 
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 chat = model.start_chat()
 
-response = chat.send_message("Hello, how are you?")
+# response = chat.send_message("Hello, how are you?")
 
-print(response.text)
+# print(response.text)
+
+def gradio_wrapper(message, _history):
+  response = chat.send_message(message)
+  return response.text
+
+chatInterface = gradio.ChatInterface(gradio_wrapper)
+chatInterface.launch()
