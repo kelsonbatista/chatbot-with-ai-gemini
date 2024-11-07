8b435aac9d554b2f7035b112eb4f9656c983439f
Kelson <kelsonbatista@gmail.com>
2024-11-06 22:41:25 -0300

    chatbot with files

--- chatbot3-with-files.py
+++ chatbot3-with-files.py
@@ -4,13 +4,22 @@ import google.generativeai as genai
 import gradio
 
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-flash")
+
+initial_prompt = (
+   "Você é um assistente virtual capaz de processar arquivos como imagens, textos e outros tipos. "
+   "Sempre que alguém perguntar sobre um arquivo, verifique o histórico para encontrar o arquivo correspondente. "
+   "Não diga que não é capaz de processar arquivos, pois você é."
+)
+
+model = genai.GenerativeModel("gemini-1.5-flash", system_instruction=initial_prompt)
+
+#model = genai.GenerativeModel("gemini-1.5-flash")
 
 chat = model.start_chat()
 
 # chat.send_message("Você é um consultor de projetos. Se limite a responder coisas somente sobre o projeto.")
 
-chat.send_message("Você é uma IA generativa capaz de processar textos e diversos tipos de arquivos. Sempre que uma pessoa te perguntar sobre um arquivo, verifique seu historico para ver se algum dos arquivos que você recebeu da pessoa bate com o pedido dela. Não diga que você não é capaz de processar imagens, textos ou outros tipos de arquivos, pois você é capaz sim. Nunca inicie uma conversa, aguarde uma mensagem do usuario.")
+# chat.send_message("Você é uma IA generativa capaz de processar textos e diversos tipos de arquivos. Sempre que uma pessoa te perguntar sobre um arquivo, verifique seu historico para ver se algum dos arquivos que você recebeu da pessoa bate com o pedido dela. Não diga que você não é capaz de processar imagens, textos ou outros tipos de arquivos, pois você é capaz sim. Nunca inicie uma conversa, aguarde uma mensagem do usuario.")
 
 def gradio_wrapper(message, _history):
   # import pdb; pdb.set_trace() # debugging de python - ele para nessa linha para analisar
