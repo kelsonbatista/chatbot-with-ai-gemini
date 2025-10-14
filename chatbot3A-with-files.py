42ba4774c14f84480c8520812b07ae8a99931fbc
Kelson <kelsonbatista@gmail.com>
2025-10-14 17:09:57 -0300

    fix: chatbot with files

---
+++ chatbot3A-with-files.py
@@ -0,0 +1,46 @@
+import os
+#import google.generativeai as genai
+from google import genai
+from google.genai import types
+import gradio
+from dotenv import load_dotenv
+
+#genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+#model = genai.GenerativeModel("gemini-1.5-flash")
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+model = "gemini-2.0-flash"
+
+client = genai.Client(api_key=GOOGLE_API_KEY)
+
+#chat = model.start_chat()
+
+#response = chat.send_message("Hello, how are you?")
+
+initial_prompt = "Você é um consultor de desenvolvimento de projetos."
+
+config = types.GenerateContentConfig(system_instruction=initial_prompt)
+
+chat = client.chats.create(model=model, config=config) #Essa função ja preserva o historico da conversa
+#chat.send_message("Hello, how are you?")
+
+def gradio_wrapper(message, _history):
+	#import pdb; pdb.set_trace() #debug
+	text = message['text']
+	uploaded_files = []
+
+	for file_path in message['files']:
+		uploaded_file = client.files.upload(file=file_path)
+		uploaded_files.append(uploaded_file)
+
+	prompt = [text]
+	prompt.extend(uploaded_files)
+	response = chat.send_message(prompt)
+	return response.text
+
+#theme = gradio.themes.Monochrome().set(mode="dark")
+
+chat_interface = gradio.ChatInterface(gradio_wrapper, type="messages", title="My Chatbot", theme='Taithrah/Minimal', multimodal=True)
+chat_interface.launch(share=False)
+#print(chat.text)
